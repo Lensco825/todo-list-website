d2de1676bc44c05900a6d2e1c6a0b66c459ea193
Lensco825 <lensdorsainvil711@outlook.com>
2025-03-02 16:35:26 -0500

    removed useState and used CSS :empty  instead.

--- src/tasksContainer.jsx
+++ src/tasksContainer.jsx
@@ -1,20 +1,16 @@
-import {useState} from 'react'
 
 function TasksContainer() {
-  const [fill, setFill] = useState('empty');
-  if (fill === 'empty') {
+
     return (
       <>
-        <div className="tasksContainer notasks">
+        <div className="tasksContainer">
+        </div>
+          <div className="notasks">
           <img src="assets/sun.png" alt="sun"/>
           <p>Start your day off productively with a task!</p>
-        </div>
+          </div>
       </>
     );
-  }
-  else {
-   return <h1>hello</h1>;
-  }
 
 }
 
