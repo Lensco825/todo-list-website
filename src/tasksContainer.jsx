c7cb173295a50e63acaa909c53f81a3ff9a63017
null <null>
2026-02-15 15:59:08 -0500

    Made tasks editable! Fixed button bug.

--- src/tasksContainer.jsx
+++ src/tasksContainer.jsx
@@ -34,41 +34,52 @@ function TasksContainer() {
   return (
     <>
 
-    <div className="taskInputContainer">
-    <h2>{greeting}</h2>
-    <p>Here are your tasks for today</p>
-    <img src="assets/sun.png" alt="sun" class="TimeImg"/>
-      <form>
-        <input type="text" minLength="3" maxLength="96" placeholder="Plan your goals here! You got this." name="taskInput" id="taskInput" onChange={e => setInfo(e.target.value)}/>
-        <label for="#taskInput" className="taskEnterBtn" onClick={() => { 
-          setTask([...tasks, {info: info}])
-          document.querySelector('#taskInput').value = null;
-          setInfo("");
+      <div className="taskInputContainer">
+        <h2>{greeting}</h2>
+        <p>Here are your tasks for today</p>
+        <img src="assets/sun.png" alt="sun" class="TimeImg" />
+        <form>
+          <input type="text" minLength="3" maxLength="96" placeholder="Plan your goals here! You got this." name="taskInput" id="taskInput" onChange={e => setInfo(e.target.value)} />
+          <label for="#taskInput" className="taskEnterBtn" onClick={() => {
+            setTask([...tasks, { info: info }])
+            document.querySelector('#taskInput').value = null;
+            setInfo("");
           }}><ion-icon name="send"></ion-icon></label>
-      </form>
-        </div>
+        </form>
+      </div>
 
       <div className="tasksContainer">
-      {tasks.map(task => (
-          <div className="task" id={"task" }>
-          <div className="textHalf">
-          <input type="checkbox"/>
-          <label className="taskContent" contentEditable="false">{task.info}</label>
-          </div>
-          <div class="taskButtons">
-            <ion-icon name="create-outline" onClick={() => {
+        {tasks.map(task => (
+          <div className="task" id={"task"}>
+            <div className="textHalf">
+              <input type="checkbox" />
+              <label className="taskContent" contentEditable="false">{task.info}</label>
+            </div>
+            <div class="taskButtons">
+              <ion-icon name="create-outline" onClick={() => {
                 index = tasks.indexOf(task);
-                if (document.querySelectorAll(".taskContent")[index].getAttribute("contentEditable") === "false") {
-                  document.querySelectorAll(".taskContent")[index].setAttribute("contentEditable", "true");
+                document.querySelectorAll(".taskContent")[index].classList.toggle("editing");
+                if (tasks.length > 1) {
+                  if (document.querySelectorAll(".taskContent")[index].getAttribute("contentEditable") === "false") {
+                    document.querySelectorAll(".taskContent")[index].setAttribute("contentEditable", "true");
+                  } else {
+                    document.querySelectorAll(".taskContent")[index].setAttribute("contentEditable", "false");
+                  }
                 } else {
-                  document.querySelectorAll(".taskContent")[index].setAttribute("contentEditable", "false");
+                  if (document.querySelector(".taskContent").getAttribute("contentEditable") === "false") {
+                    document.querySelector(".taskContent").setAttribute("contentEditable", "true");
+                  } else {
+                    document.querySelector(".taskContent").setAttribute("contentEditable", "false");
+                  }
                 }
-            }}></ion-icon>
-            <ion-icon class="deleteBtn" name="trash-outline" onClick={() => {
+
+              }}></ion-icon>
+              <ion-icon class="deleteBtn" name="trash-outline" onClick={() => {
                 index = tasks.indexOf(task);
+                tasks.splice(index, 1);
                 document.querySelectorAll('.task')[index].remove();
-            }}></ion-icon>
-        </div>
+              }}></ion-icon>
+            </div>
           </div>
         ))}
       </div>
