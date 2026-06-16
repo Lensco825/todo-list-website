be8fd38a32064116a52ed9a4d66c981dd34e3431
Lensco825 <lensdorsainvil711@outlook.com>
2026-06-16 13:06:54 -0400

    removed strict mode container

--- main.jsx
+++ main.jsx
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 import TasksContainer from './tasksContainer.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <TasksContainer />
-  </StrictMode>,
 )
