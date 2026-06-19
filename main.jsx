bbc58b6fc64528be8ed9fadd5457c3fb7c6aa9f3
Lensco825 <lensdorsainvil711@outlook.com>
2026-06-19 18:54:15 -0400

    Cleaned CSS code

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './style.css'
+import './style.css';
 import TasksContainer from './tasksContainer.jsx'
 createRoot(document.getElementById('root')).render(
     <TasksContainer />
